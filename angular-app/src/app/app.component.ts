4a824ac0522f6c62cfc9448bc2f7a9e7438ab7ee
janker6 <janker6@gmail.com>
2024-06-08 11:49:50 +0200

    ...

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StudentsComponent } from './components/students/students.component';
 import { StudentEditComponent } from './components/students-edit/students-edit.component';
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StudentsComponent, StudentEditComponent],
+  imports: [RouterOutlet, StudentsComponent, StudentEditComponent, NotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { StudentsComponent} from './components/students/students.component';
 import { StudentEditComponent } from './components/students-edit/students-edit.component';
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/students', pathMatch: 'full' },
   { path: 'students', component: StudentsComponent },
-  { path: 'edit/:id', component: StudentEditComponent }
+  { path: 'edit/:id', component: StudentEditComponent },
+
+  // new line added:
+  { path: '**', component: NotFoundComponent },
+  { path: '404', component: NotFoundComponent }
+
 ];
